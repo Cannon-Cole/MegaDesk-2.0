4fabc719619d5b40529f4fe4b66e47d678c35503
Alyssa Stevenson <hun17028@byui.edu>
2018-10-20 17:00:27 -0600

    I just noticed that these commits didn't go through on Saturday - Change file path from quotes.txt to quotes.json.

--- MegaDesk-2.0/ViewAllQuotes.cs
+++ MegaDesk-2.0/ViewAllQuotes.cs
@@ -28,7 +28,7 @@ namespace MegaDesk_4_ColeCannon
 
         private void ViewAllQuotes_Load(object sender, EventArgs e)
         {
-            StreamReader re = new StreamReader(@"quotes.txt");
+            StreamReader re = new StreamReader(@"../../assets/quotes.json");
             ViewAllDisplay.Items.Clear();
 
             try
